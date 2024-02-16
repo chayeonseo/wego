dcef52c82bc62a54bc540c21dce2c048df2bac14
sbm0709 <dustj3419@naver.com>
2024-02-16 17:42:51 +0900

    학원 240214

--- src/main/resources/static/js/main/menu.js
+++ src/main/resources/static/js/main/menu.js
@@ -1,23 +1,33 @@
 const productList = document.querySelectorAll('.menu-li');
 const editingModal = document.querySelector('.menu-editing-container')
+const editingBtn = document.querySelector('.menu-editing-btn');
 const editingCancelBtn = document.querySelector('.menu-cancel-btn');
+
 
 
 [...productList].forEach(product => {
     product.onclick = () => {
         const menuId = product.querySelector('.menuId');
+        const menuName = product.getElementsByClassName('menu-name')[0];
         editingModal.style.display = 'block';
+
+        editingBtn.onclick = () => {
+            document.querySelector('.menu-title')
+        }
 
         fetch(`/menu/${menuId.value}`)
             .then(resp => resp.json())
             .then(value => {
-                console.log(value)
+
             })
 
     }
-
 })
 
 editingCancelBtn.onclick = () => {
+
     editingModal.style.display = 'none';
+
 }
+
+
