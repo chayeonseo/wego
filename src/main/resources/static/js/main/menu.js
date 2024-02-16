019ac6af9b43a5511b3f12395f1b12b2ff3ff948
ChoiDaHui <74885951+ChoiDaHui@users.noreply.github.com>
2024-02-16 18:47:57 +0900

    커밋 ㅎ

--- src/main/resources/static/js/main/menu.js
+++ src/main/resources/static/js/main/menu.js
@@ -1,6 +1,7 @@
 const productList = document.querySelectorAll('.menu-li');
 const editingModal = document.querySelector('.menu-editing-container')
 const editingCancelBtn = document.querySelector('.menu-cancel-btn');
+const editingBtn = document.querySelector('.menu-editing-btn');
 
 
 [...productList].forEach(product => {
@@ -8,14 +9,26 @@ const editingCancelBtn = document.querySelector('.menu-cancel-btn');
         const menuId = product.querySelector('.menuId');
         editingModal.style.display = 'block';
 
+        editingBtn.onclick = () => {
+            document.querySelectorAll('.menu-editing-div input').forEach(input => input.disabled = false);
+        }
+
         fetch(`/menu/${menuId.value}`)
             .then(resp => resp.json())
             .then(value => {
-                console.log(value)
+                document.querySelector('.menu-title').value = value['menuName'];
+                document.querySelector('.menu-con').value = value['menuPrice'];
+                document.querySelector('.editing-con').value = value['menuContent'];
+                document.querySelector('.menu-editing-img').setAttribute('src', 'http://' + value['menuImgs'][0]['menuImgThm']);
+                console.log(value);
             });
     }
 });
 
 editingCancelBtn.onclick = () => {
-    editingModal.style.display = 'none';
+    if (confirm('창을 닫으시겠습니까? 저장되지 않습니다')){
+        document.querySelectorAll('.menu-editing-div input').forEach(input => input.disabled = true);
+        editingModal.style.display = 'none';
+    };
+
 }
